50e22912980401446b34226714f1b4aa74a5f57a
Diego Armando Zambrana Arze <contacto@diegozambrana.com>
2024-09-17 09:45:52 -0400

    api to get list of questions

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,4 +1,4 @@
-import { createNewQuestion } from "@/services/questions";
+import { createNewQuestion, getQuestions } from "@/services/questions";
 import { newQuestionFormat } from "@/types";
 import { NextResponse } from "next/server";
 
@@ -17,4 +17,17 @@ export const POST = async (request: Request) => {
     JSON.stringify({ message: "Question added successfully" }),
     { status: 200 }
   );
+};
+
+export const GET = async (request: Request) => {
+  const response = await getQuestions();
+  const { error, data } = response;
+
+  if (error) {
+    return new NextResponse(JSON.stringify({ message: error }), {
+      status: 400,
+    });
+  }
+
+  return new NextResponse(JSON.stringify(data), { status: 200 });
 };

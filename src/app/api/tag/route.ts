141c1ca3aa790fbe707fbb0cd88e27c58f2a5145
Diego Armando Zambrana Arze <contacto@diegozambrana.com>
2024-09-24 18:27:37 -0400

    added API to create Tags

--- src/app/api/tag/route.ts
+++ src/app/api/tag/route.ts
@@ -1,4 +1,4 @@
-import { getTags } from "@/services/tags";
+import { createTag, getTags } from "@/services/tags";
 import { NextResponse } from "next/server";
 
 export const GET = async (request: Request) => {
@@ -12,4 +12,24 @@ export const GET = async (request: Request) => {
   }
 
   return new NextResponse(JSON.stringify(data), { status: 200 });
+};
+
+export const POST = async (request: Request) => {
+  const body = await request.json();
+  const response = await createTag(body.name);
+  const { error, data } = response;
+
+  if (error) {
+    return new NextResponse(JSON.stringify({ message: error }), {
+      status: 400,
+    });
+  }
+
+  if (!data?.length) {
+    return new NextResponse(JSON.stringify({ message: "No data" }), {
+      status: 400,
+    });
+  }
+
+  return new NextResponse(JSON.stringify(data[0]), { status: 200 });
 };
--- src/services/tags.ts
+++ src/services/tags.ts
@@ -9,3 +9,22 @@ export const getTags = async () => {
 
   return { error: tagsError, data: tags || [] };
 };
+
+export const createTag = async (name: string) => {
+  const supabase = createClient();
+
+  const { data: newTag, error: tagError } = await supabase
+    .from("ruray_tag")
+    .insert({
+      name,
+      slug: name.toLowerCase().replace(" ", "-"),
+    })
+    .select("*");
+
+  console.log("Create tag");
+
+  console.log(newTag);
+  console.log(tagError);
+
+  return { error: tagError, data: newTag };
+};
